47d42761e70cde94fed0bccf60e14bd7ab7f7804
Steve Ruan <ruanzixuan318@hotmail.com>
2017-04-09 21:36:21 -0400

    4/9/2017

---
+++ TOKI_encryption/Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace TOKI_encryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.OY = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.OA1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.OB1 = new System.Windows.Forms.NumericUpDown();
+            this.OC = new System.Windows.Forms.NumericUpDown();
+            this.OD = new System.Windows.Forms.NumericUpDown();
+            this.OE = new System.Windows.Forms.NumericUpDown();
+            this.OF = new System.Windows.Forms.NumericUpDown();
+            this.OG = new System.Windows.Forms.NumericUpDown();
+            this.OH = new System.Windows.Forms.NumericUpDown();
+            this.V1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.OZ = new System.Windows.Forms.Button();
+            this.OA2 = new System.Windows.Forms.ComboBox();
+            this.OB2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.V2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.OA1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OB1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OD)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OE)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OF)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OG)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OH)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OY
+            // 
+            resources.ApplyResources(this.OY, "OY");
+            this.OY.Name = "OY";
+            this.OY.UseVisualStyleBackColor = true;
+            this.OY.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // OA1
+            // 
+            resources.ApplyResources(this.OA1, "OA1");
+            this.OA1.Maximum = new decimal(new int[] {
+            48,
+            0,
+            0,
+            0});
+            this.OA1.Minimum = new decimal(new int[] {
+            38,
+            0,
+            0,
+            0});
+            this.OA1.Name = "OA1";
+            this.OA1.Value = new decimal(new int[] {
+            38,
+            0,
+            0,
+            0});
+            this.OA1.ValueChanged += new System.EventHandler(this.OA1_ValueChanged);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // OB1
+            // 
+            resources.ApplyResources(this.OB1, "OB1");
+            this.OB1.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.OB1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.OB1.Name = "OB1";
+            this.OB1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // OC
+            // 
+            resources.ApplyResources(this.OC, "OC");
+            this.OC.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.OC.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.OC.Name = "OC";
+            this.OC.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.OC.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // OD
+            // 
+            this.OD.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            resources.ApplyResources(this.OD, "OD");
+            this.OD.Maximum = new decimal(new int[] {
+            80,
+            0,
+            0,
+            0});
+            this.OD.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.OD.Name = "OD";
+            this.OD.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // OE
+            // 
+            resources.ApplyResources(this.OE, "OE");
+            this.OE.Maximum = new decimal(new int[] {
+            35,
+            0,
+            0,
+            0});
+            this.OE.Name = "OE";
+            this.OE.Value = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            // 
+            // OF
+            // 
+            resources.ApplyResources(this.OF, "OF");
+            this.OF.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.OF.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.OF.Name = "OF";
+            this.OF.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // OG
+            // 
+            resources.ApplyResources(this.OG, "OG");
+            this.OG.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            this.OG.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.OG.Name = "OG";
+            this.OG.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // OH
+            // 
+            resources.ApplyResources(this.OH, "OH");
+            this.OH.Maximum = new decimal(new int[] {
+            99,
+            0,
+            0,
+            0});
+            this.OH.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.OH.Name = "OH";
+            this.OH.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // V1
+            // 
+            resources.ApplyResources(this.V1, "V1");
+            this.V1.Name = "V1";
+            this.V1.TextChanged += new System.EventHandler(this.UX_TextChanged);
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // OZ
+            // 
+            resources.ApplyResources(this.OZ, "OZ");
+            this.OZ.Name = "OZ";
+            this.OZ.UseVisualStyleBackColor = true;
+            this.OZ.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // OA2
+            // 
+            this.OA2.DisplayMember = "A";
+            this.OA2.FormattingEnabled = true;
+            this.OA2.Items.AddRange(new object[] {
+            resources.GetString("OA2.Items"),
+            resources.GetString("OA2.Items1"),
+            resources.GetString("OA2.Items2"),
+            resources.GetString("OA2.Items3")});
+            resources.ApplyResources(this.OA2, "OA2");
+            this.OA2.Name = "OA2";
+            this.OA2.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // OB2
+            // 
+            this.OB2.FormattingEnabled = true;
+            this.OB2.Items.AddRange(new object[] {
+            resources.GetString("OB2.Items"),
+            resources.GetString("OB2.Items1"),
+            resources.GetString("OB2.Items2"),
+            resources.GetString("OB2.Items3")});
+            resources.ApplyResources(this.OB2, "OB2");
+            this.OB2.Name = "OB2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // V2
+            // 
+            resources.ApplyResources(this.V2, "V2");
+            this.V2.Name = "V2";
+            this.V2.ReadOnly = true;
+            this.V2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.V2);
+            this.Controls.Add(this.OB2);
+            this.Controls.Add(this.OA2);
+            this.Controls.Add(this.OZ);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.V1);
+            this.Controls.Add(this.OH);
+            this.Controls.Add(this.OG);
+            this.Controls.Add(this.OF);
+            this.Controls.Add(this.OE);
+            this.Controls.Add(this.OD);
+            this.Controls.Add(this.OC);
+            this.Controls.Add(this.OB1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.OA1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OY);
+            this.Controls.Add(this.label3);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.OA1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OB1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OD)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OE)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OF)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OG)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OH)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OY;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown OA1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown OB1;
+        private System.Windows.Forms.NumericUpDown OC;
+        private System.Windows.Forms.NumericUpDown OD;
+        private System.Windows.Forms.NumericUpDown OE;
+        private System.Windows.Forms.NumericUpDown OF;
+        private System.Windows.Forms.NumericUpDown OG;
+        private System.Windows.Forms.NumericUpDown OH;
+        private System.Windows.Forms.TextBox V1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button OZ;
+        private System.Windows.Forms.ComboBox OA2;
+        private System.Windows.Forms.ComboBox OB2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox V2;
+    }
+}
+
---
+++ TOKI_encryption/Form1.cs
@@ -0,0 +1,393 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TOKI_encryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Helper function out_Of_Range_Error
+        public void out_Of_Range_Error()
+        {
+            // 重设数据
+            OA1.Value = 38;
+            OA2.Text = "A";
+            OB1.Value = 1;
+            OB2.Text = "A";
+            OC.Value = 1;
+            OD.Value = 10;
+            OE.Value = 16;
+            OF.Value = 1;
+            OG.Value = 1;
+            OH.Value = 1;
+
+            // 报错
+            V2.Text = "输入数据错误";
+        }
+
+        // Helper function trans1
+        public String trans1(int i)
+        {
+            String substr;
+            if (i < 10) substr = i.ToString();
+            else substr = ((char)(i + 55)).ToString();
+            return substr;
+        }
+
+        // Helper function trans2
+        public int trans2(char c)
+        {
+            int i = 0;
+            if (c > 64) i = (int)(c - 55);
+            else i = int.Parse(c.ToString());
+            return i;
+        }
+
+        // Helper function leap_Month_test
+        public bool leap_Month_test(int y, int m, int d)
+        {
+            return ((m < 8 && (m % 2 == 0)) || m == 9 || m == 11) && d > 30 - 2 * Convert.ToInt32(m == 2) + Convert.ToInt32(m == 2 && y % 4 == 0);
+        }
+
+        
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 长度为九的字符串
+            String str = "", substr;
+            int i;
+            if (leap_Month_test((int)OE.Value, (int)OF.Value, (int)OG.Value))
+            {
+                V1.Text = "日期错误";
+            } else
+            {
+                // 第一位
+                i = (int)OA1.Value - 37;
+                str += trans1(i);
+
+                // 第二位
+                substr = OA2.Text;
+                i = (int)(substr.ToCharArray())[0];
+                i = ((i - 65) * 10 + (int)OB1.Value);
+                str += trans1(i);
+
+                // 第三位
+                substr = OB2.Text;
+                i = (int)(substr.ToCharArray())[0];
+                i = ((i - 65) * 10 + (int)OC.Value);
+                str += trans1(i);
+
+                // 第四位
+                i = (int)OD.Value;
+                i /= 5;
+                str += trans1(i);
+
+                // 第五位
+                i = (int)OE.Value;
+                str += trans1(i);
+
+                // 第六位
+                i = (int)OF.Value;
+                str += trans1(i);
+
+                // 第七位
+                i = (int)OG.Value;
+                str += trans1(i);
+
+                // 第八位
+                i = (int)OH.Value;
+                i %= 36;
+                str += trans1(i);
+
+                // 第九位
+                i = (int)OH.Value;
+                i /= 36;
+                str += trans1(i);
+
+                // 返回转码
+                V1.Text = str;
+            }
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (V1.Text.Length != 9)
+            {
+                V2.Text = "字符串长度不符合";
+            }
+            else
+            {
+                try
+                {
+                    // 长度为十八的字符串
+                    String str = "", substr = V1.Text;
+                    Char[] ca = substr.ToCharArray();
+                    char c;
+                    int i, j;
+                    bool no_Error_Flag = true;
+
+                    // 检查所有字母是否36进制
+                    for (i = 0; i < ca.Length && no_Error_Flag; i += 1)
+                    {
+                        c = ca[i];
+                        if (c < 48 || c > 90 || (c > 57 && c < 65))
+                        {
+                            out_Of_Range_Error();
+                            no_Error_Flag = false;
+                        }
+                    }
+
+                    // 第一、二位
+                    if (no_Error_Flag)
+                    {
+                        c = ca[0];
+                        i = trans2(c) + 37;
+                        if (i > 48 || i < 38)
+                        {
+                            out_Of_Range_Error();
+                            no_Error_Flag = false;
+                        }
+                        else
+                        {
+                            OA1.Value = i;
+                            str += i.ToString();
+                        }
+                    }
+
+                    // 第三、四位
+                    if (no_Error_Flag)
+                    {
+                        c = ca[1];
+                        i = trans2(c);
+                        c = (char)(i / 10 + 65);
+                        if (c < 'A' || c > 'D' || i % 10 < 1 || i % 10 > 5)
+                        {
+                            out_Of_Range_Error();
+                            no_Error_Flag = false;
+                        }
+                        else
+                        {
+                            OA2.Text = c.ToString();
+                            OB1.Value = i % 10;
+                            substr = ((char)(i / 10 + 65)).ToString() + (i % 10).ToString();
+                            str += substr;
+                        }
+                    }
+
+                    // 第五、六位
+
+                    if (no_Error_Flag)
+                    {
+                        c = ca[2];
+                        i = trans2(c);
+                        c = (char)(i / 10 + 65);
+                        if (c < 'A' || c > 'D' || i % 10 < 1 || i % 10 > 3)
+                        {
+                            out_Of_Range_Error();
+                            no_Error_Flag = false;
+                        }
+                        else
+                        {
+                            OB2.Text = c.ToString();
+                            OC.Value = i % 10;
+                            substr = ((char)(i / 10 + 65)).ToString() + (i % 10).ToString();
+                            str += substr;
+                        }
+                    }
+
+                    // 第七、八位
+
+                    if (no_Error_Flag)
+                    {
+                        c = ca[3];
+                        i = trans2(c) * 5;
+                        if (i > 80 || i < 10)
+                        {
+                            out_Of_Range_Error();
+                            no_Error_Flag = false;
+                        }
+                        else
+                        {
+                            OD.Value = i;
+                            str += i.ToString();
+                        }
+                    }
+
+                    // 第九、十位
+
+                    if (no_Error_Flag)
+                    {
+                        c = ca[4];
+                        i = trans2(c);
+                        if (i > 35 || i < 0)
+                        {
+                            out_Of_Range_Error();
+                            no_Error_Flag = false;
+                        }
+                        else
+                        {
+                            OE.Value = i;
+                            if (i < 10) str += "0";
+                            str += i.ToString();
+                        }
+                    }
+
+                    // 第十一、十二位
+
+                    if (no_Error_Flag)
+                    {
+                        c = ca[5];
+                        i = trans2(c);
+                        if (i > 12 || i < 1)
+                        {
+                            out_Of_Range_Error();
+                            no_Error_Flag = false;
+                        }
+                        else
+                        {
+                            OF.Value = i;
+                            if (i < 10) str += "0";
+                            str += i.ToString();
+                        }
+                    }
+
+                    // 第十三、十四位
+
+                    if (no_Error_Flag)
+                    {
+                        c = ca[6];
+                        i = trans2(c);
+                        if (i > 31 || i < 1)
+                        {
+                            out_Of_Range_Error();
+                            no_Error_Flag = false;
+                        }
+                        else
+                        {
+                            OG.Value = i;
+                            if (i < 10) str += "0";
+                            str += i.ToString();
+                        }
+                    }
+
+                    // 测试闰年
+                    if (leap_Month_test((int)OE.Value, (int)OF.Value, (int)OG.Value))
+                    {
+                        out_Of_Range_Error();
+                        no_Error_Flag = false;
+                    }
+
+                    // 第十五到十八位
+                    if (no_Error_Flag)
+                    {
+                        c = ca[7];
+                        i = trans2(c);
+                        c = ca[8];
+                        j = trans2(c);
+                        if (i > 35 || i < 0 || j > 35 || j < 0)
+                        {
+                            out_Of_Range_Error();
+                            no_Error_Flag = false;
+                        }
+                        else
+                        {
+                            i += j * 36;
+                            OH.Value = i;
+                            if (i < 10) str += "000";
+                            else if (i < 100) str += "00";
+                            else if (i < 1000) str += "0";
+                            str += i.ToString();
+                        }
+                    }
+
+                    // 返回转码
+                    if (no_Error_Flag) V2.Text = str;
+                }
+                catch (Exception ex) 
+                {
+                    out_Of_Range_Error();
+                }
+            }
+
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UX_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OA1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
